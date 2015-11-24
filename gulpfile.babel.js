7cc67ce1b483994d735b59b62a632c98b1266436
Yuki Ito <yuito@shiguredo.jp>
2015-11-24 17:16:46 +0900

    gulpfile を追加する

---
+++ gulpfile.babel.js
@@ -0,0 +1,63 @@
+import browserify from "browserify";
+import gulp from "gulp";
+import eslint from "gulp-eslint";
+import uglify from "gulp-uglify";
+import rename from "gulp-rename";
+import header from "gulp-header";
+import runSequence from "run-sequence";
+import buffer from "vinyl-buffer";
+import source from "vinyl-source-stream";
+
+import pkg from "./package.json";
+
+
+gulp.task("dist", callback => {
+  return runSequence.use(gulp)(
+      "compile",
+      "header",
+      "uglify",
+      callback
+      );
+});
+
+gulp.task("compile", () => {
+  return browserify({
+    entries: "anzu.js",
+    debug: false,
+    standalone: "Anzu"
+  })
+  .transform("babelify")
+  .bundle()
+  .pipe(source("anzu.js"))
+  .pipe(buffer())
+  .pipe(gulp.dest("dist"));
+});
+
+gulp.task("uglify", () => {
+  return gulp.src(["dist/anzu.js"])
+    .pipe(uglify({preserveComments: "some"}))
+    .pipe(rename({extname: ".min.js"}))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("header", () => {
+  let banner = `
+/*!
+ * <%= pkg.name %>
+ * <%= pkg.description %>
+ * @version <%= pkg.version %>
+ * @author <%= pkg.author %>
+ * @license <%= pkg.license %>
+ */
+`;
+  return gulp.src(["dist/anzu.js"])
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("eslint", () => {
+  return gulp.src(["anzu.js"])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
