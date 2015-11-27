85d1d0aae3dbef737d23da8d1b2ddf524e804f3c
Yuki Ito <yuito@shiguredo.jp>
2015-11-27 16:31:52 +0900

    fixture を使ったテストのサンプル実装を作成する

--- test/anzu.spec.js
+++ test/anzu.spec.js
@@ -1,9 +1,29 @@
-/* global describe:true it:true */
+/* global describe:true it:true before:true */
 import assert from "power-assert";
+import Anzu from "../anzu";
 
 describe("Anzu", () => {
   describe("startUpstream", () => {
-    it("example", () => {
+    before(() => {
+      document.body.innerHTML = window.__html__["test/fixtures/localvideo.html"];
+    });
+    it("startUpstream", (done) => {
+      let anzu = new Anzu();
+      anzu.startUpstream(
+        "sora",
+        "",
+        {video: true},
+        document.getElementById("local-video"),
+        function() {
+          done();
+        },
+        function(_e) {
+          done();
+        },
+        function(_e) {
+          done();
+        }
+      );
     });
   });
 });
