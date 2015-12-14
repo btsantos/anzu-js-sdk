0e9e4cf8dc7fe15fbe7b3ba015f09c34727bab0e
Yuki Ito <yuito@shiguredo.jp>
2015-12-14 19:37:20 +0900

    1 object で up/down の 1 connect しかできないようにする

--- anzu.js
+++ anzu.js
@@ -14,226 +14,147 @@ class Anzu {
   /**
    * @constructor
    */
-  constructor(params={anzuUrl: null, soraUrl: null}) {
-    this.url = params.anzuUrl === null ? "https://anzu.shiguredo.jp/api/" : params.anzuUrl;
-    this.sora = new Sora(params.soraUrl === null ? "wss://anzu.shiguredo.jp/signaling" : params.soraUrl);
-    this.upstreamPc;
-    this.downstreamPc = {};
+  constructor(role, params={anzuUrl: null, soraUrl: null}) {
+    this.anzuUrl = params.anzuUrl === null ? "https://anzu.shiguredo.jp/api/" : params.anzuUrl;
+    this.soraUrl = params.soraUrl === null ? "wss://anzu.shiguredo.jp/signaling" : params.soraUrl;
+    if (role !== "upstream" && role !== "downstream") {
+      let error = new Error("Role " + role + " is not defined");
+      throw error;
+    }
+    this.role = role;
+  }
+  start(channelId, token, constraints=null) {
+    if (this.role === "upstream") {
+      let c = constraints === null ? { video: true, audio: true } : constraints;
+      return this._startUpstream(channelId, token, c);
+    }
+    else if (this.role === "downstream") {
+      return this._startDownstream(channelId, token);
+    }
   }
   /**
    * アップストリームを開始する
    * @param {string} channelId - チャンネルID
    * @param {string} upstreamToken - アップストリームトークン
    * @param {object} constraints - LocalMediaStream オブジェクトがサポートするメディアタイプ
-   * @param {object} videoElement - ストリームをプレイするビデオエレメント
-   * @param {onSuccessCallback} onSuccess - 接続成功時のコールバック
-   * @param {onErrorCallback} onError - エラー時のコールバック
-   * @param {onCloseCallback} onClose - 接続切断時のコールバック
-   * @example
-   * var anzu = new Anzu();
-   * anzu.startUpstream(
-   *   "channelId",
-   *   "token",
-   *   {video: true},
-   *   document.getElementById("local-video"),
-   *   function(clientId) {
-   *     // success
-   *   },
-   *   function(error) {
-   *     // error
-   *   },
-   *   function(error) {
-   *     // close
-   *   }
    * )
    */
-  startUpstream(channelId, upstreamToken, constraints, videoElement, onSuccess, onError, onClose) {
-    let _getUserMedia = (constraints) => {
+  _startUpstream(channelId, upstreamToken, constraints) {
+    let getUserMedia = (constraints) => {
       return new Promise((resolve, reject) => {
         if (navigator.getUserMedia) {
-          navigator.getUserMedia(constraints,
-            (stream) => {
-              resolve(stream);
-            },
-            (err) => {
-              reject(err);
-            }
-          );
+          navigator.getUserMedia(constraints, (stream) => {
+            this.stream = stream;
+            resolve({stream: stream});
+          }, (err) => { reject(err); });
         } else {
           reject(message);
         }
       });
     };
-    let _createPeerConnection = (params) => {
-      this.sdplog("Upstream Offer", params.offer);
-      let offer = params.offer;
-      let stream = params.stream;
-      return new Promise((resolve, reject) => {
-        let pc = new RTCPeerConnection({iceServers: offer.iceServers});
-        pc.addStream(stream);
-        resolve({pc: pc, offer: offer});
+    let createOffer = () => {
+      this.sora = new Sora(this.soraUrl).connection();
+      return this.sora.connect({
+        role: "upstream",
+        channelId: channelId,
+        accessToken: upstreamToken
       });
     };
-    let _createAnswer = (params) => {
-      let pc = params.pc;
-      let offer = params.offer;
+    let createPeerConnection = (offer) => {
+      this.sdplog("Upstream Offer", offer);
       return new Promise((resolve, reject) => {
-        pc.setRemoteDescription(new RTCSessionDescription(offer), () => {
-          pc.createAnswer((answer) => {
-            this.sdplog("Upstream answer", params.offer);
-            resolve({pc: pc, answer: answer, offer: offer});
+        this.clientId = offer.clientId;
+        this.pc = new RTCPeerConnection({iceServers: offer.iceServers});
+        this.pc.addStream(this.stream);
+        resolve(offer);
+      });
+    };
+    let createAnswer = (offer) => {
+      return new Promise((resolve, reject) => {
+        this.pc.setRemoteDescription(new RTCSessionDescription(offer), () => {
+          this.pc.createAnswer((answer) => {
+            this.sdplog("Upstream answer", offer);
+            this.pc.setLocalDescription(answer, () => {
+              this.sora.answer(answer.sdp);
+              this.pc.onicecandidate = (event) => {
+                if (event.candidate !== null) {
+                  console.info("====== candidate ======");
+                  console.info(event.candidate);
+                  this.sora.candidate(event.candidate);
+                }
+              };
+              resolve({clientId: this.clientId, stream: this.stream});
+            }, (error) => { reject(error); });
           }, (error) => { reject(error); });
         }, (error) => { reject(error); });
       });
     };
-    let connection = this.sora.connection(
-      () => {
-        _getUserMedia(constraints)
-          .then((stream) => {
-            return new Promise((resolve, reject) => {
-              videoElement.src = window.URL.createObjectURL(stream);
-              videoElement.play();
-              let params = {role: "upstream", channelId: channelId, accessToken: upstreamToken};
-              connection.connect(
-                params,
-                (offer) => { resolve({offer: offer, stream: stream}); },
-                (error) => { reject(error); }
-              );
-            });
-          })
-          .then(_createPeerConnection)
-          .then(_createAnswer)
-          .then((params) => {
-            return new Promise((resolve, reject) => {
-              let pc = params.pc;
-              let answer = params.answer;
-              let offer = params.offer;
-              pc.setLocalDescription(answer, () => {
-                connection.answer(answer.sdp);
-                this.upstreamPc = pc;
-                resolve(params.offer.clientId)
-                pc.onicecandidate = (event) => {
-                  if (event.candidate !== null) {
-                    console.info("====== candidate ======");
-                    console.info(event.candidate);
-                    connection.candidate(event.candidate);
-                  }
-                };
-              }, (error) => { reject(error); });
-            });
-          })
-          .then((clientId) => {
-            onSuccess(clientId);
-          })
-          .catch((error) => {
-            onError(error);
-          });
-      },
-      onError,
-      (e) => {
-        videoElement.pause();
-        videoElement.src = "";
-        connection = null;
-        this.upstreamPc = null;
-        onClose(e);
-      }
-    );
+    return getUserMedia(constraints)
+      .then(createOffer)
+      .then(createPeerConnection)
+      .then(createAnswer);
   }
   /**
    * ダウンストリームを開始する
    * @param {string} channelId - チャンネルID
    * @param {string} downstreamToken - ダウンストリームトークン
-   * @param {object} videoElement - ストリームをプレイするビデオエレメント
-   * @param {onSuccessCallback} onSuccess - 接続成功時のコールバック
-   * @param {onErrorCallback} onError - エラー時のコールバック
-   * @param {onCloseCallback} onClose - 接続切断時のコールバック
-   * @example
-   * var anzu = new Anzu();
-   * anzu.startDownstream(
-   *   "channelId",
-   *   "token",
-   *   document.getElementById("remote-video"),
-   *   function(clientId) {
-   *     // success
-   *   },
-   *   function(error) {
-   *     // error
-   *   },
-   *   function(error) {
-   *     // close
-   *   }
-   * )
    */
-  startDownstream(channelId, downstreamToken, videoElement, onSuccess, onError, onClose) {
-    let _createPeerConnection = (params) => {
-      this.sdplog("Downstream offer", params.offer);
-      return new Promise((resolve, reject) => {
-        let offer = params.offer;
-        let pc = new RTCPeerConnection({iceServers: offer.iceServers});
-        resolve({pc: pc, offer: offer});
+  _startDownstream(channelId, downstreamToken) {
+    let createOffer = () => {
+      this.sora = new Sora(this.soraUrl).connection();
+      console.log("-- DOWNSTREAM OFFER --");
+      return this.sora.connect({
+        role: "downstream",
+        channelId: channelId,
+        accessToken: downstreamToken
       });
     };
-    let _createAnswer = (params) => {
-      let pc = params.pc;
-      let offer = params.offer;
+    let createPeerConnection = (offer) => {
+      this.sdplog("Downstream offer", offer);
       return new Promise((resolve, reject) => {
-        pc.setRemoteDescription(new RTCSessionDescription(offer), () => {
-          pc.createAnswer((answer) => {
-            this.sdplog("Downstream answer", params.offer);
-            resolve({pc: pc, offer: offer, answer: answer});
-          }, (error) => { reject(error); });
-        }, (error) => { reject(error); });
-        pc.onaddstream = (event) => {
-          videoElement.src = window.URL.createObjectURL(event.stream);
-          videoElement.play();
-        };
+        this.clientId = offer.clientId;
+        this.pc = new RTCPeerConnection({iceServers: offer.iceServers});
+        resolve(offer);
       });
     };
-    let connection = this.sora.connection(
-      () => {
-        new Promise((resolve, reject) => {
-          let params = {role: "downstream", channelId: channelId, accessToken: downstreamToken};
-          connection.connect(
-            params,
-            (offer) => { resolve({offer: offer}); },
-            (error) => { reject(error); }
-          );
-        })
-        .then(_createPeerConnection)
-        .then(_createAnswer)
-        .then((params) => {
-          return new Promise((resolve, reject) => {
-            let pc = params.pc;
-            let answer = params.answer;
-            let clientId = params.offer.clientId;
-            pc.setLocalDescription(answer, () => {
-              connection.answer(answer.sdp);
-              this.downstreamPc[clientId] = pc;
-              resolve(clientId)
-              pc.onicecandidate = (event) => {
+    let createAnswer = (offer) => {
+      // firefox と chrome のタイミング問題判定用 flag
+      let is_ff = navigator.mozGetUserMedia !== undefined;
+      return new Promise((resolve, reject) => {
+        if (!is_ff) {
+          this.pc.onaddstream = (event) => {
+            this.stream = event.stream;
+          };
+        }
+        this.pc.setRemoteDescription(new RTCSessionDescription(offer), () => {
+          this.pc.createAnswer((answer) => {
+            this.sdplog("Downstream answer", offer);
+            this.pc.setLocalDescription(answer, () => {
+              this.sora.answer(answer.sdp);
+              this.sendanswer = true;
+              if (is_ff) {
+                this.pc.onaddstream = (event) => {
+                  this.stream = event.stream;
+                  resolve({clientId: this.clientId, stream: this.stream});
+                };
+              }
+              else {
+                resolve({clientId: this.clientId, stream: this.stream});
+              }
+              this.pc.onicecandidate = (event) => {
                 if (event.candidate !== null) {
-                  console.info("====== candidate ======");
                   console.info(event.candidate);
-                  connection.candidate(event.candidate);
+                  this.sora.candidate(event.candidate);
                 }
               };
-            }, onError);
-          });
-        })
-        .then((clientId) => {
-          onSuccess(clientId);
-        })
-        .catch((error) => {
-          onError(error);
-        });
-      },
-      onError,
-      (e) => {
-        videoElement.pause();
-        videoElement.src = "";
-        onClose(e);
-      }
-    );
+            }, (error) => { reject(error); });
+          }, (error) => { reject(error); });
+        }, (error) => { reject(error); });
+      });
+    };
+    return createOffer()
+      .then(createPeerConnection)
+      .then(createAnswer);
   }
   sdplog(title, target) {
     console.info("========== " + title + " ==========");
