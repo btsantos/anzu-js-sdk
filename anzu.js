2e04bb084a5157cd47de07c1a367b07a52cd1fbd
Yuki Ito <yuito@shiguredo.jp>
2015-12-15 10:42:06 +0900

    jsdoc を修正する

--- anzu.js
+++ anzu.js
@@ -6,7 +6,10 @@ navigator.getUserMedia = navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia;
-
+/**
+ * This is Anzu JavaScript SDK
+ * @class Anzu
+ */
 class Anzu {
   /**
    * @constructor
