c4b4a4df704271111db0b34165dad27953fc8e5e
Yuki Ito <yuitowest@gmail.com>
2016-03-23 14:35:43 +0900

    gulpfile を更新する

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,50 +1,65 @@
-import fs from "fs";
-import browserify from "browserify";
 import gulp from "gulp";
-import jsdoc2md from "gulp-jsdoc-to-markdown";
-import eslint from "gulp-eslint";
+import del from "del";
 import uglify from "gulp-uglify";
 import rename from "gulp-rename";
 import header from "gulp-header";
+import eslint from "gulp-eslint";
+import plumber from "gulp-plumber";
+import watchify from "gulp-watchify";
 import runSequence from "run-sequence";
 import buffer from "vinyl-buffer";
-import source from "vinyl-source-stream";
-
-
 import pkg from "./package.json";
 
 
 gulp.task("dist", callback => {
   return runSequence.use(gulp)(
-      "compile",
-      "uglify",
+      "clean",
+      "compile:js",
       "header",
+      "uglify",
       callback
       );
 });
 
-gulp.task("compile", () => {
-  return browserify({
-    entries: "anzu.js",
-    debug: false,
-    standalone: "Anzu"
-  })
-  .transform("babelify")
-  .bundle()
-  .pipe(source("anzu.js"))
-  .pipe(buffer())
-  .pipe(gulp.dest("dist"));
+gulp.task("clean", () => {
+  return del([
+    "dist/**/*"
+  ]);
+});
+
+let watching = false
+gulp.task("enable-watch-mode", () => {
+  watching = true;
+});
+
+gulp.task("compile:js", watchify((w) => {
+  return gulp.src("src/anzu.js")
+    .pipe(plumber())
+    .pipe(w({
+      watch: watching,
+      transform: ["babelify", "envify"],
+      standalone: "Anzu"
+    }))
+    .pipe(buffer())
+    .pipe(gulp.dest("dist"));
+}));
+
+gulp.task("watchify", ["enable-watch-mode", "compile:js"])
+
+gulp.task("watch", ["watchify"], function () {
+  gulp.watch("dist/anzu.js", ["uglify"]);
 });
 
 gulp.task("uglify", () => {
   return gulp.src(["dist/anzu.js"])
-    .pipe(uglify())
-    .pipe(rename({extname: ".min.js"}))
+    .pipe(uglify({ preserveComments: "some" }))
+    .pipe(rename({ extname: ".min.js" }))
     .pipe(gulp.dest("dist"));
 });
 
 gulp.task("header", () => {
-  let banner = `/**
+  let banner = `
+/*!
  * <%= pkg.name %>
  * <%= pkg.description %>
  * @version <%= pkg.version %>
@@ -52,24 +67,14 @@ gulp.task("header", () => {
  * @license <%= pkg.license %>
  */
 `;
-  return gulp.src(["dist/anzu.js", "dist/anzu.min.js"])
-    .pipe(header(banner, {pkg: pkg}))
+  return gulp.src(["dist/anzu.js"])
+    .pipe(header(banner, { pkg: pkg }))
     .pipe(gulp.dest("dist"));
 });
 
 gulp.task("eslint", () => {
-  return gulp.src(["anzu.js"])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError());
-});
-
-gulp.task("jsdoc", () => {
-  return gulp.src(["anzu.js"])
-  .pipe(jsdoc2md({  }))
-  .pipe(rename(function(path){
-    path.extname = ".md";
-    path.basename = "API";
-  }))
-  .pipe(gulp.dest("."));
+  return gulp.src(["src/anzu.js"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
